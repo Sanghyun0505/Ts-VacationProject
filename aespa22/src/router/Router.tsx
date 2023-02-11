c956af9b432841b481bbacef6c0c6fe20076d795
박상현 <104070112+Sanghyun0505@users.noreply.github.com>
2023-02-11 20:02:12 +0900

    ⚡️ :: 로그인 라우터 추가

--- aespa22/src/router/Router.tsx
+++ aespa22/src/router/Router.tsx
@@ -1,10 +1,11 @@
 import { Routes, Route } from "react-router-dom";
+import Login from "../components/login";
 import HomePage from "../pages/HomePage";
-
 export default function Router(){
     return(
         <Routes>
             <Route path="/" element={<HomePage/>}/>
+            <Route path="/login" element={<Login/>}/>
         </Routes>
     );
 }
