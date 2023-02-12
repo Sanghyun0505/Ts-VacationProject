a41d0f4b6958d893aefb213967dba9803d46f3bb
박상현 <104070112+Sanghyun0505@users.noreply.github.com>
2023-02-12 22:48:03 +0900

    ⚡️:: 회원가입페이지 라우터 추가

--- aespa22/src/router/Router.tsx
+++ aespa22/src/router/Router.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "../components/login";
+import SignUp from "../components/signup";
 import HomePage from "../pages/HomePage";
 export default function Router(){
     return(
         <Routes>
             <Route path="/" element={<HomePage/>}/>
             <Route path="/login" element={<Login/>}/>
+            <Route path='/signup' element={<SignUp/>}/>
         </Routes>
     );
 }
